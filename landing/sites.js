fa02371eb051032e1f9884a76baa9c0af369204e
pixelkd <129384325+pixelkd@users.noreply.github.com>
2025-04-07 00:02:28 -0500

    new structure

---
+++ landing/sites.js
@@ -0,0 +1,26 @@
+const sites = [
+    {
+        title: "Storyboard Portfolio",
+        link: "/story", 
+        image: "landing/assets/storypage.png",
+        description: "A portfolio showcasing my work as a storyboard artist, including a collection of my storyboards and some comics pages.",
+    }
+];
+
+const container = document.querySelector("#site-container");
+
+sites.forEach(site => {
+    const card = document.createElement("div");
+    card.className = "site-card";  
+
+    card.innerHTML = `
+        <img src="${site.image}" alt="${site.title}">
+        <div class="content">
+            <h2>${site.title}</h2>
+            <p>${site.description}</p>
+            <a href="${site.link}" target="_blank" rel="noopener">Open in new tab →</a>
+        </div>
+    `;
+
+    container.appendChild(card);
+});
